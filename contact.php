630517369d4eb2d2812f08998b5ed4b14ee51d82
Pau Siquier <pausiqrey8@gmail.com>
2022-07-12 18:44:16 +0200

    Initial commit

---
+++ contact.php
@@ -0,0 +1,70 @@
+<!-- Contact -->
+
+  <div class="contact">
+    <div class="contact_container">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="section_title text-center">
+              <div>Salem Fish</div>
+              <br>
+              <h1>Contact Us</h1>
+              <br>
+            </div>
+            <div class="contact_text text-center">
+              <p>Please if you have any questions do not hesitate to contact me or send me an email info@salemfish.com</p>
+            <br>
+            </div> 
+            <div class="contact_form_container">
+              <form action="#" class="contact_form d-flex flex-column align-items-center justify-content-center">
+                <div class="row d-flex flex-column align-items-center justify-content-center">
+                  <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
+                    <input type="text" class="contact_input d-flex flex-column align-items-center justify-content-center" placeholder="Your name" required="required">
+                  </div>
+                  <br>
+                  <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
+                    <input type="email" class="contact_input d-flex flex-column align-items-center justify-content-center" placeholder="Your email" required="required">
+                  </div>
+                  <br>
+                </div>
+                <input type="text" class="contact_input d-flex flex-column align-items-center justify-content-center" placeholder="Subject">
+                <br>
+                <textarea class="contact_input d-flex flex-column align-items-center justify-content-center" placeholder="Message" required="required"></textarea>
+                <br>
+                <button class="contact_button d-flex flex-column align-items-center justify-content-center">__Send__</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Map -->
+    <div class="contact_map_container">
+      <div class="map">
+        <div id="google_map" class="google_map">
+          <div class="map_container">
+            <div id="map"></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Contact Map Content -->
+      <div class="contact_map_content">
+        <div class="d-flex flex-column align-items-center justify-content-center">
+          <img class="contact_info_logo_1" src="images/logo_4.png" alt="">
+          <img class="contact_info_logo_2" src="images/logo_2.png" alt="">
+          <div class="contact_info_list">
+            <ul class="text-center">
+            <br>
+              <li>راس الحد,Main Road Al Hadd, Oman</li>
+              <li><a href="https://www.google.com/maps/place/Salem+Fish+Guesthouse/@22.5260912,59.789026,233m/data=!3m1!1e3!4m5!3m4!1s0x3e9a017d5b7b9bef:0xcb517b7eff5082b0!8m2!3d22.5259798!4d59.7886888?hl=en-DE">Google Maps Link</a><li>
+              <li>info@salemfish.com</li>
+              <li>+968 9595 1944 </li>
+            </ul>
+            <br>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
---
+++ largeimg.php
@@ -0,0 +1,24 @@
+  
+            <!-- Contact Details Column -->
+            <div class="col-lg-8">
+                <h3>Contact Details</h3>
+                <p>
+                >راس الحد,Main Road<br Al Hadd, Oman<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>: +968 9595 1944</p>
+                <p><i class="fa fa-envelope-o"></i> 
+                    <abbr title="Email">E</abbr>: <a href="emailto:name@example.com">info@salemfish.com</a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Sunday: 9:00 AM to 5:00 PM</p>
+                <ul class="list-unstyled list-inline list-social-icons">
+                <li><a href="https://www.instagram.com/salemfish/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                <li><a href="https://www.facebook.com/salem.fish"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+              <div class="col-lg-4"> 
+        <div class="row">
+          <?php  require_once('sidebar.php') ; ?>
+        </div>
+      </div>
