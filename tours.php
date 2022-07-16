e0d0d3c3815f9da09c91d98b6981597bcb56ceb3
Pau Siquier <pausiqrey8@gmail.com>
2022-07-16 11:15:40 +0200

    posicion check in/out
    posicionar check in y check out junto al boton book now . falta responsive

--- tours.php
+++ tours.php
@@ -11,13 +11,13 @@
               <h1>Tours</h1>
             </div>
             <div class="intro_text">
-                <p>Tours will be paid by hand at our GuestHouse in OmaniRial, You don't need to book here, but you can always email us for additional info. Places guaranteed!<p>
-                <p> Book a boat trip directly from us, for fishing, dolphin-watching and snorkeling; Catch of the 
+                <p><b>You don't need to book here, places guaranteed if you sleep in our guest house!</b> Tours will be <b>paid by hand</b> at our GuestHouse in OmaniRial, but you can always email us for additional info. <p>
+                <p>Book a boat trip directly from us <b>without intermediaries</b>, for fishing, dolphin-watching and snorkeling; Catch of the 
                   day BBQ also available for an unforgettable dinner Bedouin-style, with local fresh fish. 
-                </p><p> We also do guided desert tours 
+                </p><p>We also do guided <b>desert tours</b> 
                     4x4, camp, accommodation and food included just contact me for more information and availability. 
-                     I hope you will make many
-                     memories here and we look forward to welcoming you soon!</p>
+                    </p><p >I hope you will make many
+                     memories here and we look forward to <b>welcoming you soon!</b></p>
             </div>
       </div>
     </div>
@@ -32,7 +32,7 @@
             <div class="offer_content text-center">
               <div class="offer_title"><h3>Turtle</h3></div>
               <div class="offer_text">
-                <p>Ras al Hadd it is the best enclave to see green turtles. You will see them just walking along the beach almost all year round!</p>
+                <p>Ras al Hadd it is <b>the best</b> enclave to see green turtles. You will see them just walking along the beach almost all year round!</p>
               </div>
             <!--   <div class="offer_button"><a href="#">discover</a></div> -->
             </div>
@@ -46,7 +46,7 @@
             <div class="offer_content text-center">
               <div class="offer_title"><h3>Dolphin Tour</h3></div>
               <div class="offer_text">
-                <p>Dozens of dolphins will follow the boat jumping. Our clients say that it is the best experience of their trip in Oman and one of the best of their life.</p>
+                <p>Dozens of dolphins will follow the boat jumping. Our clients say that it is <b>the best experience</b> of their trip in Oman and one of the best of their <b>life</b>.</p>
               </div>
             <!--   <div class="offer_button"><a href="#">discover</a></div> -->
             </div>
@@ -60,7 +60,7 @@
             <div class="offer_content text-center">
               <div class="offer_title"><h3>Fishing Tour</h3></div>
               <div class="offer_text">
-              <p>It does not matter if you are an expert Angler or want to fish for the first time. You will fish Omani style.</p>
+              <p>It does not matter if you are an expert Angler or want to fish for the first time. You will fish<b> Omani style</b>.</p>
                </div>
             <!--   <div class="offer_button"><a href="#">discover</a></div> -->
             </div>
@@ -74,8 +74,8 @@
             <div class="offer_content text-center">
               <div class="offer_title"><h3>Omani BBQ for dinner</h3></div>
               <div class="offer_text">
-              <p>We prepare it Omani style! Salem will go to the market to look for Lobsters, Tuna, Lamb... or you will eat fishing tour's Catch of the 
-                  day BBQ All very fresh and delicious. If you have any intolerance or you are Vegan/Vegetarian, let us know in advance.</p>
+              <p>We prepare it <b>Omani style!</b> Salem will go to the market to look for Lobsters, Tuna, Lamb... or you will eat fishing tour's <b>Catch of the 
+                  day</b> BBQ All very fresh and delicious. If you have any intolerance or you are Vegan/Vegetarian, let us know <b>in advance.</b></p>
               </div>
             </div>
           </div>
@@ -86,9 +86,9 @@
           <div class="offer">
             <div class="offer_image"><img src="images/offer_6.jpg" alt=""></div>
             <div class="offer_content text-center">
-              <div class="offer_title"><h3>Desert Tour</h3></div>
+              <div class="offer_title"><h3>Desert Tour </h3></div>
               <div class="offer_text">
-                <p>You will sleep in the desert enjoying millions of stars. we take care of everything, rooms, campfire, dinner, so you have a real experience in an Omani desert. Please book in advance</p>
+                <p>You will sleep in the desert enjoying millions of stars. we take care of everything, rooms, campfire, dinner, so you have a real experience in an Omani desert. <b>Please book in advance by mail</b></p>
               </div>
             </div>
           </div>
