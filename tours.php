5b71b463f7798073323c355d0eb7e4b756b85936
Pau Siquier <pausiqrey8@gmail.com>
2022-07-17 21:35:35 +0200

    Tabla precios y estilos dorados
    tabla de precios y cambio de los estilos para unificar mas los colores

--- tours.php
+++ tours.php
@@ -8,10 +8,10 @@
           <div class="intro_content">
             <div class="section_title text-center">
               <div>Tours</div>
-              <h1>Tours</h1>
+              <h1>Book your tours directly at the Guest House!</h1>
             </div>
             <div class="intro_text">
-                <p><b>You don't need to book here, places guaranteed if you sleep in our guest house!</b> Tours will be <b>paid by hand</b> at our GuestHouse in OmaniRial, but you can always email us for additional info. <p>
+                <p><b>You don't need to book here, places guaranteed if you sleep in our guest house!</b> Tours will be <b>paid in Omani Rial (no credit card)</b>, but you can always email us for additional info if you like. <p>
                 <p>Book a boat trip directly from us <b>without intermediaries</b>, for fishing, dolphin-watching and snorkeling; Catch of the 
                   day BBQ also available for an unforgettable dinner Bedouin-style, with local fresh fish. 
                 </p><p>We also do guided <b>desert tours</b> 
@@ -28,7 +28,7 @@
         <!-- Offer Item -->
         <div class="col-xl-4 col-md-6">
           <div class="offer">
-            <div class="offer_image"><img src="images/offer_2.jpg" alt=""></div>
+            <div class="offer_image"><img src="images/offer_2.jpg" alt="Turtle"></div>
             <div class="offer_content text-center">
               <div class="offer_title"><h3>Turtle</h3></div>
               <div class="offer_text">
@@ -42,7 +42,7 @@
         <!-- Offer Item -->
         <div class="col-xl-4 col-md-6">
           <div class="offer">
-            <div class="offer_image"><img src="images/offer_3.jpg" alt=""></div>
+            <div class="offer_image"><img src="images/offer_3.jpg" alt="Dolphin Tour"></div>
             <div class="offer_content text-center">
               <div class="offer_title"><h3>Dolphin Tour</h3></div>
               <div class="offer_text">
@@ -56,7 +56,7 @@
         <!-- Offer Item -->
         <div class="col-xl-4 col-md-6">
           <div class="offer">
-            <div class="offer_image"><img src="images/offer_4.jpg" alt=""></div>
+            <div class="offer_image"><img src="images/offer_4.jpg" alt="Fishing Tour"></div>
             <div class="offer_content text-center">
               <div class="offer_title"><h3>Fishing Tour</h3></div>
               <div class="offer_text">
@@ -70,7 +70,7 @@
         <!-- Offer Item -->
         <div class="col-xl-4 col-md-6">
           <div class="offer">
-            <div class="offer_image"><img src="images/offer_5.jpg" alt=""></div>
+            <div class="offer_image"><img src="images/offer_5.jpg" alt="Omani BBQ for dinner"></div>
             <div class="offer_content text-center">
               <div class="offer_title"><h3>Omani BBQ for dinner</h3></div>
               <div class="offer_text">
@@ -84,9 +84,9 @@
         <!-- Offer Item -->
         <div class="col-xl-4 col-md-6">
           <div class="offer">
-            <div class="offer_image"><img src="images/offer_6.jpg" alt=""></div>
+            <div class="offer_image"><img src="images/offer_6.jpg" alt="Desert Tour"></div>
             <div class="offer_content text-center">
-              <div class="offer_title"><h3>Desert Tour </h3></div>
+              <div class="offer_title"><h3>Desert Tour</h3></div>
               <div class="offer_text">
                 <p>You will sleep in the desert enjoying millions of stars. we take care of everything, rooms, campfire, dinner, so you have a real experience in an Omani desert. <b>Please book in advance by mail</b></p>
               </div>
@@ -101,49 +101,47 @@
   </div>  
 
 <!-- Tours Price list -->
-
-  <div class="row text-center" >
-  <div class="container_price_list ">
-      <h1 class="text-center "> Tours Price List</h1>
-    
-      
-        <h3>BBQ Fish Dinner</h3>
-       
-          
-            <h6>5 OMR per Adult</h6>
-            <h6>3 OMR per Child<</h6>
-         
-        
-    
-      
-        <h3>Dolphin Tour</h3>
-          
-          <h6>35 OMR for 3 persons and young families</h6>
-            <h6>+10 OMR each for 4 or more persons</h6>
-          
-        <h3>Fishing Tour</h3>
-          
-          <h6>35 OMR for 3 persons and young families</h6>
-            <h6>+10 OMR each for 4 or more persons</h6>
-          
-      
-        <h3>Snorkelling Tour</h3>
-         
-            <h6>35 OMR for 3 persons and young families</h6>
-            <h6>+10 OMR each for 4 or more persons</h6>
-          
-        <h3>Turtle Tour</h3>
-        
-            <h6>My friend Rasaljin will do this tour<h6>
-            <h6>He is the only guide in charge in Ras Al Hadd doing all the turtle tours in the town.
-               He will give you price and schedule. just ask me about him and i I will put you in contact with him for free.</h6>
-       
-
-    
-  </div>
-</div>
-   
   
+<div class="table-title">
+<h3 id="table_h3">Tours Price List</h3>
+</div>
+<table class="table-fill">
+<thead>
+<tr>
+<th class="text-left">Tours</th>
+<th class="text-left">Prices</th>
+</tr>
+</thead>
+<tbody class="table-hover">
+<tr>
+<td class="text-left">BBQ Fish Dinner</td>
+<td class="text-left">5 OMR per Adult / 3 OMR per Child.</td>
+</tr>
+<tr>
+<td class="text-left">Dolphin Tour</td>
+<td class="text-left">35 OMR for 3 persons and young families <br>+10 OMR each for 4 or more persons.</td>
+</tr>
+<tr>
+<td class="text-left">Fishing Tour</td>
+<td class="text-left">35 OMR for 3 persons and young families <br>+10 OMR each for 4 or more persons.</td>
+</tr>
+<tr>
+<td class="text-left">Snorkelling Tour</td>
+<td class="text-left">35 OMR for 3 persons and young families <br>+10 OMR each for 4 or more persons.</td>
+</tr>
+<tr>
+<tr>
+<td class="text-left">Turtle Tour</td>
+<td class="text-left">My friend Rasaljin will do this tour, he is the only guide in charge in Ras Al Hadd doing all the turtle tours in the town.
+               He will give you price and schedule. Just ask and i I will put you in contact with him.</td>
+</tr>
+<td class="text-left">Desert Tour</td>
+<td class="text-left">PLEASE book in advance by mail. Price will vary according to how many you are.</td>
+</tr>
+</tbody>
+</table>
+<br>
+
   
   
   
